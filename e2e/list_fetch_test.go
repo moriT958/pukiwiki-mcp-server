28d736fd4a93eb8081abc63d35c1b69a4b95f47f
morita <morit958@gmail.com>
2026-02-28 01:12:36 +0900

    e2e test

---
+++ e2e/list_fetch_test.go
@@ -0,0 +1,49 @@
+package e2e
+
+import (
+	"os"
+	"testing"
+
+	"github.com/moriT958/pukiwiki-mcp/libpuki"
+)
+
+func TestListThenGetSource(t *testing.T) {
+	baseURL := os.Getenv("PUKIWIKI_URL")
+	user := os.Getenv("PUKIWIKI_USER")
+	pass := os.Getenv("PUKIWIKI_PASS")
+	scope := os.Getenv("PUKIWIKI_SCOPE")
+
+	if baseURL == "" {
+		t.Skip("PUKIWIKI_URL not set")
+	}
+
+	c, err := libpuki.New(baseURL,
+		libpuki.WithAuth(user, pass),
+		libpuki.WithScope(scope),
+	)
+	if err != nil {
+		t.Fatalf("New: %v", err)
+	}
+
+	if err := c.Login(); err != nil {
+		t.Fatalf("Login: %v", err)
+	}
+
+	pages, err := c.ListPages()
+	if err != nil {
+		t.Fatalf("ListPages: %v", err)
+	}
+	if len(pages) == 0 {
+		t.Fatal("ListPages returned no pages")
+	}
+	t.Logf("Found %d page(s): %v", len(pages), pages)
+
+	source, err := c.GetPageSource(pages[3])
+	if err != nil {
+		t.Fatalf("GetPageSource(%q): %v", pages[0], err)
+	}
+	if source == "" {
+		t.Errorf("GetPageSource(%q) returned empty source", pages[0])
+	}
+	t.Logf("Source of %q:\n%s", pages[0], source)
+}
