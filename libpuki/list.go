8ed01cc851c2fbaf94434a074898f3380ea6c2f8
morita <morit958@gmail.com>
2026-02-28 01:09:13 +0900

    ListPages method

---
+++ libpuki/list.go
@@ -0,0 +1,35 @@
+package libpuki
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// PukiWiki 内ののページ一覧を返す
+// WithScope が設定されている場合、そのプレフィックス配下のページのみを返す
+func (c *Client) ListPages() ([]string, error) {
+	reqURL := fmt.Sprintf("%s/?cmd=list", c.baseURL)
+
+	doc, err := c.fetchDocument(reqURL)
+	if err != nil {
+		return nil, err
+	}
+
+	var pages []string
+	doc.Find("#body ul li ul li a").Each(func(_ int, s *goquery.Selection) {
+		name := strings.TrimSpace(s.Text())
+		if name == "" {
+			return
+		}
+		if c.scope != "" {
+			if name != c.scope && !strings.HasPrefix(name, c.scope+"/") {
+				return
+			}
+		}
+		pages = append(pages, name)
+	})
+
+	return pages, nil
+}
